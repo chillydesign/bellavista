b5ed35461939935d167f1f7d2e33e46c31e9df11
Charles Harvey <harvey.charles@gmail.com>
2019-03-10 17:41:49 +0100

    test coordinates

--- sections/map.php
+++ sections/map.php
@@ -1,6 +1,7 @@
 <?php $coordinates = (get_sub_field('lat_lng'));  ?>
 <?php $marker_title = strval(get_sub_field('marker_title'));  ?>
 <?php $latlng = explode(',',  $coordinates); ?>
+<?php var_dump($coordinates); ?>
 <?php var_dump($latlng); ?>
 <?php $lat = $latlng[0]; ?>
 <?php $lng = $latlng[1]; ?>
