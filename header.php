0a9e52e7991ae1d9556aacf253465f58da71b8f4
Charles Harvey <harvey.charles@gmail.com>
2019-04-26 10:30:48 +0200

    add social links to header

--- header.php
+++ header.php
@@ -2,46 +2,46 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <?php $tdu = get_template_directory_uri(); ?>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-	<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="//www.google-analytics.com" rel="dns-prefetch">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/img/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/img/favicons/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo $tdu; ?>/img/favicons/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo $tdu; ?>/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-TileImage" content="<?php echo $tdu; ?>/img/favicons/mstile-144x144.png">
-    <meta name="theme-color" content="#ffffff"></head>
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tdu; ?>/img/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tdu; ?>/img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tdu; ?>/img/favicons/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo $tdu; ?>/img/favicons/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo $tdu; ?>/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-TileImage" content="<?php echo $tdu; ?>/img/favicons/mstile-144x144.png">
+        <meta name="theme-color" content="#ffffff">
 
-
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="<?php bloginfo('description'); ?>">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,700,700i" rel="stylesheet">
-	<?php wp_head(); ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,700,700i" rel="stylesheet">
+        <?php wp_head(); ?>
 
 
-</head>
-<body <?php body_class(); ?>>
+    </head>
+    <body <?php body_class(); ?>>
 
-<?php // do_action('wpml_add_language_selector'); ?>
-	<!-- HEADER -->
-	<header id="header">
-		<div class="container">
-			<a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
-			<nav>
-				<ul>
-					<?php chilly_nav('header_menu'); ?>
-				</ul>
-			</nav>
-			<div class="clear"></div>
-			<a href="#" id="show_mobile_nav" >Menu</a>
-		</div>
+        <?php // do_action('wpml_add_language_selector'); ?>
+        <!-- HEADER -->
+        <header id="header">
+            <div class="container">
+                <a href="<?php echo home_url(); ?>" class="branding"><?php bloginfo('name'); ?></a>
+                <nav>
+                    <ul>
+                        <?php chilly_nav('header_menu'); ?>
+                        <li class="facebook"><a href="https://www.facebook.com/lesvillasdesgrumes/">Facebook</a></li>
+                        <li class="youtube"><a href="https://www.youtube.com/channel/UCOacTZLBUOj7vYxHk8qavGA?view_as=subscriber">Youtube</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+                <a href="#" id="show_mobile_nav" >Menu</a>
+            </div>
 
-	</header>
+        </header>
